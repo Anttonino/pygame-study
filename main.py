2b84a98ab8436cfa2274a7487a295ea3d93382e0
Anttonino <victor.antonino12@gmail.com>
2023-03-06 23:00:01 -0300

    :sparkles: feat: control object move

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.init ()
 width = 640
 height = 480
 x = width / 2
-y = 0 
+y = height / 2
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Display Config')
@@ -21,10 +21,29 @@ while True:
     for event in pygame.event.get ():
         if event.type == QUIT:
             pygame.quit()
-            exit() 
+            exit()
+        """"
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                x -= 1
+            if event.key == K_d:
+                x += 1
+            if event.key == K_w:
+                y -= 1
+            if event.key == K_s:
+                y += 1
+        """
+        if pygame.key.get_pressed()[K_a]:
+            x -= 1
+        if pygame.key.get_pressed()[K_d]:
+            x += 1
+        if pygame.key.get_pressed()[K_w]:
+            y -= 1
+        if pygame.key.get_pressed()[K_s]:
+            y += 1
+
+        
+
     pygame.draw.rect(screen, (255,0,0), (x, y, 40,50))
-    if y >= height:
-        y = 0
-    y = y + 1 
-    
+
     pygame.display.update ()
