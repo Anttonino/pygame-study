d3b0969943b3ae2cfb41077ab37f055555a44613
Anttonino <victor.antonino12@gmail.com>
2023-03-06 23:27:27 -0300

    :sparkles: feat: create new object with collision

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pygame
-
 from pygame.locals import * 
-
 from sys import exit
+from random import randint
 
 pygame.init ()
 
@@ -11,12 +10,15 @@ height = 480
 x = width / 2
 y = height / 2
 
+x_obstacle = randint (40, 600)
+y_obstacle = randint (50, 430)
+
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Display Config')
 clock = pygame.time.Clock()
 
 while True:
-    clock.tick(30)
+    clock.tick(60)
     screen.fill((0, 0, 0))
     for event in pygame.event.get ():
         if event.type == QUIT:
@@ -31,19 +33,22 @@ while True:
             if event.key == K_w:
                 y -= 1
             if event.key == K_s:
-                y += 1
+                y += 1    
         """
         if pygame.key.get_pressed()[K_a]:
-            x -= 1
+            x -= 10
         if pygame.key.get_pressed()[K_d]:
-            x += 1
+            x += 10
         if pygame.key.get_pressed()[K_w]:
-            y -= 1
+            y -= 10
         if pygame.key.get_pressed()[K_s]:
-            y += 1
+            y += 10
 
-        
+    palyer = pygame.draw.rect(screen, (255,0,0), (x, y, 40,50))
+    obstacle = pygame.draw.rect(screen, (0,0,255), (x_obstacle, y_obstacle, 40,50))
 
-    pygame.draw.rect(screen, (255,0,0), (x, y, 40,50))
+    if palyer.colliderect (obstacle):
+        x_obstacle = randint(40, 600)
+        y_obstacle = randint(50, 430)
 
     pygame.display.update ()
