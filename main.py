706fa17a6cee2caff3b6fef39d5dd62b9dfedd72
Anttonino <victor.antonino12@gmail.com>
2023-03-07 16:38:49 -0300

    :sparkles: feat: create a game score

--- main.py
+++ main.py
@@ -5,36 +5,31 @@ from random import randint
 
 pygame.init ()
 
-width = 640
-height = 480
+size = width, height = 640, 480
+black = 0, 0 ,0
+
 x = width / 2
 y = height / 2
-
 x_obstacle = randint (40, 600)
 y_obstacle = randint (50, 430)
 
-screen = pygame.display.set_mode((width, height))
+score = 0
+font = pygame.font.SysFont('arial', 40, True, True)
+
+screen = pygame.display.set_mode((size))
 pygame.display.set_caption('Display Config')
 clock = pygame.time.Clock()
 
-while True:
+while True: 
     clock.tick(60)
-    screen.fill((0, 0, 0))
+    screen.fill((black))
+    message = f'Score: {score}'
+    score_render = font.render(message, True, (255,255,255))
     for event in pygame.event.get ():
         if event.type == QUIT:
             pygame.quit()
             exit()
-        """"
-        if event.type == KEYDOWN:
-            if event.key == K_a:
-                x -= 1
-            if event.key == K_d:
-                x += 1
-            if event.key == K_w:
-                y -= 1
-            if event.key == K_s:
-                y += 1    
-        """
+
         if pygame.key.get_pressed()[K_a]:
             x -= 10
         if pygame.key.get_pressed()[K_d]:
@@ -50,5 +45,7 @@ while True:
     if palyer.colliderect (obstacle):
         x_obstacle = randint(40, 600)
         y_obstacle = randint(50, 430)
-
+        score += 1
+    
+    screen.blit(score_render, (430,40))
     pygame.display.update ()
