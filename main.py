18175141cea0b0c6d81ad8504c98d1073b9f6a72
Anttonino <victor.antonino12@gmail.com>
2023-03-08 14:11:14 -0300

    :sparkles: feat: create a size for the player object
    the player will increase its length while walking
    
    :recycle: refactor: change player and obstacle variable name to snake and apple

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ point_effect = pygame.mixer.Sound('assets/point.wav')
 
 # Display Size
 size = width, height = 640, 480
-black = 0, 0 ,0
+white = 255, 255, 255
 
 # Positions
-x = width / 2
-y = height / 2
-x_obstacle = randint (40, 600)
-y_obstacle = randint (50, 430)
+x_snake = width / 2
+y_snake = height / 2
+x_apple = randint (40, 600)
+y_apple = randint (50, 430)
 
 # Score 
 score = 0
@@ -33,15 +33,20 @@ screen = pygame.display.set_mode((size))
 pygame.display.set_caption('Display Config')
 clock = pygame.time.Clock()
 
+snake_length = []
+def growth (snake_length):
+    for XY in snake_length:
+        pygame.draw.rect(screen, (0,255,0), (XY[0], XY[1], 20, 20))
+
 # Primary Loop (The Game)
 while True:
     # Framaskip 
     clock.tick(60)
     # Screen Color
-    screen.fill((black))
+    screen.fill((white))
     # Game Score
     message = f'Score: {score}'
-    score_render = font.render(message, True, (255,255,255))
+    score_render = font.render(message, True, (0,0,0))
     # Secondary loop (Check events in game)
     for event in pygame.event.get ():
         # Quit Game
@@ -50,22 +55,30 @@ while True:
             exit()
         # Input Config
         if pygame.key.get_pressed()[K_a]:
-            x -= 10
+            x_snake -= 5
         if pygame.key.get_pressed()[K_d]:
-            x += 10
+            x_snake += 5
         if pygame.key.get_pressed()[K_w]:
-            y -= 10
+            y_snake -= 5
         if pygame.key.get_pressed()[K_s]:
-            y += 10
+            y_snake += 5
     # Objects
-    palyer = pygame.draw.rect(screen, (255,0,0), (x, y, 40,50))
-    obstacle = pygame.draw.rect(screen, (0,0,255), (x_obstacle, y_obstacle, 40,50))
+    snake = pygame.draw.rect(screen, (0,255,0), (x_snake, y_snake, 20,20))
+    apple = pygame.draw.rect(screen, (255,0,0), (x_apple, y_apple, 20,20))
     # Collision between player and obstacle
-    if palyer.colliderect (obstacle):
-        x_obstacle = randint(40, 600)
-        y_obstacle = randint(50, 430)
+    if snake.colliderect (apple):
+        x_apple = randint(40, 600)
+        y_apple = randint(50, 430)
         score += 1
         point_effect.play()
+
+    head_length = []
+    head_length.append(x_snake)
+    head_length.append(y_snake)
+    snake_length.append(head_length)
+
+    growth (snake_length)
+
     # Score position in screen
     screen.blit(score_render, (430,40))
     # Refresh the screen while the primary loop receives new information
